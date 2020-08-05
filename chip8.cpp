d3aeb9db158cd11b0b7b12bd3f34885f191bdb95
Nathan Moses <nathanmoses13@gmail.com>
2020-08-05 19:54:40 +0100

    Added mock test file and implemented main function to process input files

--- chip8.cpp
+++ chip8.cpp
@@ -1,5 +1,42 @@
-//function to process op codes
+//main function to read in CHIP-8 programs
+int main(int argc, char** argv)
+{
+	FILE* f = fopen(argv[1], "rb");
+	if (f == NULL)
+	{
+		printf("error: Couldn't open %s\n", argv[1]);
+		exit(1);
+	}
 
+	//Get the files size
+	fseek(f, 0L, SEEK_END);
+	int fsize = ftell(f);
+	fseek(f, 0L, SEEK_SET);
+
+	//CHIP-8 convention puts programs in memory at 0x200
+	//They will all have hardcoded addresses expecting that
+	//
+	//Read the file into memory at 0x200 and close it
+
+	unsigned char* buffer = malloc(fsize + 0x200);
+	fread(buffer + 0x200, fsize, 1, f);
+	fclose(f);
+
+	//program counter set at 0x200 address
+	int pc = 0x200;
+	while (pc < fsize + 0x200) )
+	{
+	DisassembleChip8Op(buffer, pc);
+	pc += 2;							//increment by 2 bytes
+	printf("\n");
+
+	}
+	return 0;
+}
+
+
+
+//function to process op codes
 void DisassembleChip8Op(uint8_t* codebuffer, int pc)
 {
 	uint8_t* code = &codebuffer[pc];	//??
@@ -16,7 +53,7 @@ void DisassembleChip8Op(uint8_t* codebuffer, int pc)
 		case 0x04: printf("4 not handled yet"); break;
 		case 0x05: printf("5 not handled yet"); break;
 
-		// 0x6XNN movesan immediate value to a register
+		// op code 6XNN movesan immediate value to a register
 		case 0x06:
 		{
 			uint8_t reg = code[0] & 0x0f;
@@ -24,5 +61,24 @@ void DisassembleChip8Op(uint8_t* codebuffer, int pc)
 		}
 		break;
 		case 0x07: printf("7 not handled yet"); break;
+		case 0x08: printf("8 not handled yet"); break;
+		case 0x09: printf("9 not handled yet"); break;
+
+		// op code ANNN : moves an address to the memory address register I
+		case 0x0a:
+		{
+			uint8_t addresshi = code[0] & 0x0f;
+			printf("%-10s I,#$%01x02x", "MVI", addresshi, code[1]);
+	
+		}
+		break;
+
+		case 0x0b: printf("b not handles yet"); break;
+		case 0x0c: printf("c not handles yet"); break;
+		case 0x0d: printf("d not handles yet"); break;
+		case 0x0e: printf("e not handles yet"); break;
+		case 0x0f: printf("f not handles yet"); break;
+
+
 	}
 }
